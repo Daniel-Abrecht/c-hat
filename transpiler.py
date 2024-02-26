cb32ab3e52836bec7c9a4480a18c371fc0916d2f
Daniel Abrecht <public@danielabrecht.ch>
2024-02-26 20:10:37 +0000

    Add ignored tokens back directly after parsing instead of when serializing

--- transpiler.py
+++ transpiler.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import sys
-from lark import Lark, Visitor, Tree, Token
+from lark import Lark, Tree, Token, Transformer
 
 with open("syntax.lark") as f:
   class C_hat:
@@ -8,21 +8,24 @@ with open("syntax.lark") as f:
     def __init__(self, code):
       self.code = code
       self.tree = C_hat.parser.parse(code)
+      self.add_back_ignored_tokens()
       self.transpile()
     def transpile(self):
-      # TODO
+      #print(self.tree.pretty())
       pass
-    def dump_code(self):
+    def add_back_ignored_tokens(self):
       context = type('', (), {})()
       context.pos = 0
       def sub(tree):
-        #print(tree.data);
-        for entry in tree.children:
+        n = 0
+        for i, entry in enumerate(tree.children):
           if isinstance(entry,Tree):
             sub(entry)
           elif isinstance(entry, Token):
-            spaces = self.code[context.pos:entry.start_pos]
-            print(f"{spaces}{entry}", end='')
+            if context.pos < entry.start_pos:
+              spaces = self.code[context.pos:entry.start_pos]
+              tree.children.insert(i+n,Token('ignored', spaces))
+              n += 1
             context.pos = entry.end_pos
           else:
             raise Exception("Can't handle this type of entry");
@@ -30,9 +33,18 @@ with open("syntax.lark") as f:
       spaces = self.code[context.pos]
       if spaces[-1] != '\n':
         spaces += '\n'
-      print(spaces, end='')
+      self.tree.children.append(Token('ignored', spaces))
+    def dump_code(self):
+      def sub(tree):
+        for entry in tree.children:
+          if isinstance(entry,Tree):
+            sub(entry)
+          elif isinstance(entry, Token):
+            print(entry, end='')
+          else:
+            raise Exception("Can't handle this type of entry");
+      sub(self.tree)
+
 
 with open(sys.argv[1]) as f:
   C_hat(f.read()).dump_code()
-
-# TODO: make changes here
