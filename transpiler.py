75ece6ee8f30d8b1306f03f9b408a4ab9d2c117a
Daniel Abrecht <public@danielabrecht.ch>
2024-02-28 22:06:51 +0000

    wip

--- transpiler.py
+++ transpiler.py
@@ -2,6 +2,12 @@
 import sys
 from lark import Lark, Tree, Token, Transformer, Visitor
 
+def ast(tree):
+  if isinstance(tree, list):
+    return [x for x in [ast(x) for x in tree] if x]
+  elif isinstance(tree, Tree):
+    return getattr(tree.meta,'ast', None)
+
 with open("syntax.lark") as f:
   class C_hat:
     parser = Lark(f.read(), start="translation_unit", keep_all_tokens=True)
@@ -11,10 +17,8 @@ with open("syntax.lark") as f:
       self.add_back_ignored_tokens()
       self.transpile()
     def transpile(self):
-      ParallelAst().visit(self.tree)
+      ShadowAst().visit(self.tree)
       #print(self.tree.pretty())
-      self.tree = TransformVariableArrayArguments().transform(self.tree)
-      pass
     def add_back_ignored_tokens(self):
       context = type('', (), {})()
       context.pos = 0
@@ -38,79 +42,107 @@ with open("syntax.lark") as f:
         spaces += '\n'
       self.tree.children.append(Token('ignored', spaces))
 
-def serialize(tree):
-  result = ''
-  for entry in tree.children:
-    if isinstance(entry,Tree):
-      if hasattr(entry.meta, 'ast'):
-        result += entry.meta.ast.serialize()
-      else:
-        result += serialize(entry)
-    elif isinstance(entry, Token):
-      result += entry
+def serialize(x):
+  if isinstance(x, Tree):
+    a = ast(x)
+    if a:
+      return a.serialize()
     else:
-      raise Exception("Can't handle this type of entry");
-  return result
+      result = ''
+      for entry in x.children:
+        result += serialize(entry)
+      return result
+  elif isinstance(x, Token):
+    return x
+  raise Exception("Can't handle this type of entry");
 
-class ParallelAstEntry():
+class ShadowAstEntry():
   def __init__(self, tree):
     tree.meta.ast = self
     self.tree = tree
-  def parent():
+  def parent(self):
     t = self.tree
     while True:
-      t = getattr(t, 'parent')
+      t = getattr(t, 'parent', None)
       if not t: break
-      ast = getattr(t.meta, 'ast')
-      if ast:
-        return ast
-  def ast_children():
+      a = ast(t)
+      if a:
+        return a
+  def closest(self, name):
+    a = self
+    while True:
+      a = a.parent()
+      if not a:
+        break
+      if a.data == name:
+        return a
+  def ast_children(self):
     l = []
     def sub(tree):
       for entry in tree.children:
         if isinstance(entry,Tree):
-          if hasattr(entry.meta, 'ast'):
-            l.append(entry.meta.ast)
+          a = ast(entry)
+          if a:
+            l.append(a)
           else:
             sub(entry)
+    sub(self.tree)
     return l
   def serialize(self):
-    return serialize(self.tree)
+    result = ''
+    for entry in self.tree.children:
+      result += serialize(entry)
+    return result
 
-class ParallelAst(Visitor):
-  class declaration_specifiers(ParallelAstEntry):
+class declarator_mixin: pass
+class direct_declarator_mixin: pass
+class array_declarator_mixin: pass
+class function_declarator: pass
+
+class ShadowAst(Visitor):
+  class declaration_specifiers(ShadowAstEntry):
     def __init__(self, tree):
       super().__init__(tree)
     def get_specifiers(self):
       return [str(token) for token in self.tree.children if isinstance(token, Token) and token.type != 'ignored']
-  class direct_declarator(ParallelAstEntry):
-    def __init__(self, tree):
-      super().__init__(tree)
-    def get_identifier(self):
-      first_rule = next(x for x in self.tree.children if isinstance(x, Tree))
-      a = getattr(first_rule.meta, 'ast')
-      if isinstance(a, ParallelAst.identifier):
-        return a.value()
-      elif isinstance(a, direct_declarator):
-        return a.get_identifier()
-  class direct_array_declarator(direct_declarator):
-    def __init__(self, tree):
-      super().__init__(tree)
-  class identifier(ParallelAstEntry):
-    def __init__(self, tree):
-      super().__init__(tree)
-    def value(self):
-      return next(x for x in self.tree.children if isinstance(x, Token) and x.type == 'IDENTIFIER')
 
-class TransformVariableArrayArguments(Transformer):
-  def parameter_list(self, args):
-    for entry in args:
-      if isinstance(entry, Tree):
-        if entry.data == 'parameter_declaration':
-          #print(entry.pretty())
-          #print(entry.data)
-          pass
-    return Tree('parameter_list', args)
+  class declarator(ShadowAstEntry, declarator_mixin):
+    def get_identifier(self):
+      for a in ast(self.tree.children):
+        if isinstance(a, ShadowAst.identifier):
+          return a.value()
+        elif isinstance(a, ShadowAst.declarator):
+          return a.get_identifier()
+  class abstract_declarator(ShadowAstEntry, declarator_mixin): pass
+
+  class direct_declarator(declarator, direct_declarator_mixin): pass
+  class direct_abstract_declarator(abstract_declarator, direct_declarator_mixin): pass
+
+  class direct_array_declarator(direct_declarator, array_declarator_mixin): pass
+  class abstract_array_declarator(direct_abstract_declarator, array_declarator_mixin): pass
+
+  class direct_function_declarator(direct_declarator, function_declarator): pass
+  class abstract_function_declarator(direct_abstract_declarator, function_declarator): pass
+
+  class identifier(ShadowAstEntry):
+    def value(self):
+      return next(x for x in self.tree.children if isinstance(x, Token) and x.type == 'IDENTIFIER').value
+
+  class parameter_declaration(declarator):
+    pass
+
+  class parameter_list(ShadowAstEntry):
+    def get_parameters(self):
+      return [entry for entry in ast(self.tree.children) if isinstance(entry, ShadowAst.parameter_declaration)]
+    def get_arguments(self):
+      return [p.get_identifier() for p in self.get_parameters()]
+    def serialize(self):
+      args = self.get_arguments()
+      result = ''
+      for i, entry in enumerate(self.tree.children):
+        defined_later = args[i+1:]
+        result += serialize(entry)
+      return result
 
 with open(sys.argv[1]) as f:
   print(serialize(C_hat(f.read()).tree))
